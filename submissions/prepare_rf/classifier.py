2f9e83149487c425d1bb8f79295b7b626b5b9f99
titigmr <thierry.gmr@outlook.fr>
2021-06-19 14:30:28 +0200

    add process time

---
+++ submissions/prepare_rf/classifier.py
@@ -0,0 +1,16 @@
+from lightgbm import LGBMClassifier
+
+class Classifier:
+
+    def __init__(self):
+        self.clf = LGBMClassifier(
+            n_estimators=50, max_depth=20, random_state=44, n_jobs=-1)
+        print(self.clf)
+
+    def fit(self, X_source, X_source_bkg, X_target, X_target_unlabeled,
+            X_target_bkg, y_source, y_target):
+        self.clf.fit(X_source, y_source)
+
+    def predict_proba(self, X_target, X_target_bkg):
+        y_proba = self.clf.predict_proba(X_target)
+        return y_proba
