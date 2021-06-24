9c6218db29d1e5749245115d3ca4205a51211f5b
aflatoune <daniel.kharsa@gmail.com>
2021-06-24 18:29:55 +0200

    modify classifier

--- submissions/prepare_rf/classifier.py
+++ submissions/prepare_rf/classifier.py
@@ -1,10 +1,11 @@
 from lightgbm import LGBMClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 class Classifier:
 
     def __init__(self):
-        self.clf = LGBMClassifier(
-            n_estimators=200, max_depth=20, random_state=44, n_jobs=-1)
+        self.clf = RandomForestClassifier(
+            n_estimators=500, max_depth=25, random_state=44, n_jobs=-1)
         print(self.clf)
 
     def fit(self, X_source, X_source_bkg, X_target, X_target_unlabeled,
