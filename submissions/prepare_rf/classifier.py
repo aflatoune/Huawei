701a85d434d35430ed6531c215b96388cb9d1584
titigmr <thierry.gmr@outlook.fr>
2021-06-26 22:40:10 +0200

    submission

--- submissions/prepare_rf/classifier.py
+++ submissions/prepare_rf/classifier.py
@@ -1,12 +1,14 @@
 from lightgbm import LGBMClassifier
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+from sklearn.feature_selection import SelectFromModel
+
 
 class Classifier:
 
     def __init__(self):
         self.clf = LGBMClassifier(
-            n_estimators=500, max_depth=-1, random_state=44, n_jobs=-1)
+            n_estimators=2000, max_depth=-1, random_state=44, n_jobs=-1)
         print(self.clf)
 
     def fit(self, X_source, X_source_bkg, X_target, X_target_unlabeled,
@@ -14,8 +16,12 @@ class Classifier:
 
         X = np.vstack([X_source, X_target])
         y = np.hstack([y_source, y_target])
+
+        selector = SelectFromModel(threshold='0.2*mean', estimator=LGBMClassifier()).fit(X, y)
+        self.col_selected = selector.get_support()
+        X = X[:, self.col_selected]
         self.clf.fit(X, y)
 
     def predict_proba(self, X_target, X_target_bkg):
-        y_proba = self.clf.predict_proba(X_target)
+        y_proba = self.clf.predict_proba(X_target[:, self.col_selected])
         return y_proba
--- submissions/prepare_rf/feature_extractor.py
+++ submissions/prepare_rf/feature_extractor.py
@@ -10,14 +10,17 @@ class FeatureExtractor:
 
     def transform(self, X):
         if not (len(X) == 29592) and (not len(X) == 50862):
+            if self.count >= 1:
+                self.count = 1
             cleaner = DataCleaner(drop_olt_recv=True)
             X = cleaner.clean_data(X)
             prep = PrepareExtractor()
             X, _ = prep.get_data(X,
                                  col_names=cleaner.columns,
                                  size_sample=-1,
-                                 resample={"unit": 'H', 'func': 'mean'},
-                                 slice_=-24, name=self.count)
+                                 resample={'func': 'mean', 'unit': 'H'},
+                                 slice_=[(12, 36), (-24, None)],
+                                 name=self.count)
 
             self.count += 1
         print(X.shape)
@@ -306,15 +309,29 @@ class PrepareExtractor:
             X = self._resampling(X=X, resample=resample,
                                  columns=col_names, verbose=verbose)
         if first_diff:
-            X = self.add_first_diff(X, n_diff=first_diff)
+            X =  self.add_first_diff(X, n_diff=first_diff)
 
         if slice_:
-            X = X.iloc[:slice_, :]
+            len_index = len(X)
+            slice_ = self.get_slicing(len_index=len_index, liste_borne=slice_)
+            X = X.iloc[slice_, :]
 
         if add_unit:
             add_unit = self._get_add_unit(add_unit)
             X = X.apply(self.get_unit, **add_unit, axis=1)
         return X, y
+
+    def get_slicing(self, len_index, liste_borne):
+        slicing_ = []
+        for sl in liste_borne:
+            if len(sl) == 2:
+                s, e = sl
+                p = 1
+            else:
+                s, e, p = sl
+            slicing_ += list(range(len_index))[s:e][::p]
+        return slicing_
+
 
     def add_first_diff(self, X, n_diff):
         X = pd.concat([X[n_diff:], X.diff(n_diff)[n_diff:]], axis=1)
